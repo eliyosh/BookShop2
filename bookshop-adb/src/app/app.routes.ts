fc0238b843fb799dac7ab3857cf0e80ab042a654
eliyosh <20246799@s.uabguio.edu>
2026-06-25 11:26:29 +0800

    feat: updated the routes

--- bookshop-adb/src/app/app.routes.ts
+++ bookshop-adb/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { NewBooks } from './new-books/new-books';
+import { Category } from './category/category';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Home },
+  { path: 'pages', component: Category },
+  { path: 'order', component: NewBooks },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
