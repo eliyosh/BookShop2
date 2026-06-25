9aa4557e098fe438b432e17258f1b6e5e71c4fdb
eliyosh <20246799@s.uabguio.edu>
2026-06-25 11:15:09 +0800

    feat: added the inject cartService to home.ts

--- bookshop-adb/src/app/home/home.ts
+++ bookshop-adb/src/app/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject} from '@angular/core';
+import {cartService} from '../cart';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  cartService = inject (cartService);
+}
