63395aadb07c7e5980d34d67f2d9edc5895dbd84
eliyosh <20246799@s.uabguio.edu>
2026-06-25 11:23:54 +0800

    feat: added inject in new-books

--- bookshop-adb/src/app/new-books/new-books.ts
+++ bookshop-adb/src/app/new-books/new-books.ts
@@ -7,3 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './new-books.css',
 })
 export class NewBooks {}
+import { Component, inject, signal } from '@angular/core';
+import { CartService } from '../cart';
+
+@Component({
+  selector: 'app-order',
+  standalone: true,
+  templateUrl: './new-books.html',
+  styleUrl: './new-books.css'
+})
+export class NewBooks {
+  cartService = inject(CartService);
+
+//validation
+  customerData = signal({ name: '', phone: '', address: '' });
+
+  updateField(field: string, event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.customerData.update(current => ({ ...current, [field]: value }));
+  }
+
+  processReceipt() {
+    const form = this.customerData();
+    if (!form.name || !form.phone || !form.address) {
+      alert('Please fill out all missing kiosk delivery validation inputs!');
+      return;
+    }
+    
+    alert(`Order Confirmed!\n\nThank you, ${form.name}.\nYour invoice total of ₱${this.cartService.totalPrice()} has been logged securely.\nDeliveries will dispatch directly to: ${form.address}.`);
+    this.cartService.clearCart();
+  }
+}
