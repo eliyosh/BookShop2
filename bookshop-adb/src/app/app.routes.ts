434cd1da7c7b9e0f44236d519ec925c84b9b00d2
eliyosh <20246799@s.uabguio.edu>
2026-07-06 09:58:33 +0800

    feat: added library component to routes

--- bookshop-adb/src/app/app.routes.ts
+++ bookshop-adb/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { NewBooks } from './new-books/new-books';
 import { Category } from './category/category';
+import { LibraryComponent } from './library/library';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'pages', component: Category },
   { path: 'order', component: NewBooks },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'library', component: LibraryComponent}
 ];
